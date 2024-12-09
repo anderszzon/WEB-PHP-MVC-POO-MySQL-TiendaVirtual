1f1b3fea8928f78fa9f5398c62e059f4eff6a088
Anderson Gordillo <mauri201111@gmail.com>
2024-12-09 10:07:17 -0500

    Initial commit

---
+++ Conexion.php
@@ -0,0 +1,25 @@
+<?php
+
+	class Conexion{
+		private $host = "localhost";
+		private $user = "root";
+		private $password = "";
+		private $db = "db_sistema";
+		private $conect;
+
+		public function __construct(){
+			$connectionString = "mysql:host=".$this->host.";dbname=".$this->db.";charset=utf8";
+			try{
+				$this->conect = new PDO($connectionString, $this->user, $this->password);
+				$this->conect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				echo "Successfull Connection";
+			}catch(PDOException $e){
+				$this->conect = 'Error de conexión';
+			    echo "ERROR: " . $e->getMessage();
+			}
+		}
+	}
+
+	$conect = new Conexion();
+
+?>
